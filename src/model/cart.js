edd8356625bf80b89f0dfef8ae3d4899d68ba49a
henriquehschmitt <henriquehschmitt@hotmail.com>
2022-02-12 02:14:32 -0300

    feat: added cart schema

---
+++ src/model/cart.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const cartSchema = new mongoose.Schema({
+    subtotal: {
+        type: Number,
+        required: true
+    },
+    discount: {
+        type: Number,
+        required: true
+    },
+    taxes: {
+        type: Number,
+        required: true
+    },
+    total: {
+        type: Number,
+        required: true
+    },
+    items: {
+        type: Array,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('cart', cartSchema)
