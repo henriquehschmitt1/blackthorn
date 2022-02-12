d494ac171eda7b2acf14fcbbf1a8250c53071c67
henriquehschmitt <henriquehschmitt@hotmail.com>
2022-02-11 21:23:44 -0300

    feat: added prettier config file

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 4,
+  trailingComma: "none",
+  arrowParens: "avoid",
+  printWidth: 120,
+  endOfLine: "auto",
+};
