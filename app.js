edc4107f47c18d8f2c8a96cbfec7cae1c9f40605
henriquehschmitt <henriquehschmitt@hotmail.com>
2022-02-13 20:32:18 -0300

    fix: now using the correct service to post a new item

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 require('./src/config/database').connect()
 const express = require('express')
+const ItemService = require('./src/services/item-service')
 
 const app = express()
 
 app.use(express.json())
 
-app.post('/item', File.registerFile)
+app.post('/item', ItemService.createItem)
 
 module.exports = app
