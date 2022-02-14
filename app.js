fee315be5045e55f589373a1c6fe1279815e795d
henriquehschmitt <henriquehschmitt@hotmail.com>
2022-02-14 19:37:07 -0300

    feat: added a route to add items to the cart

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 require('./src/config/database').connect()
 const express = require('express')
+const CartController = require('./src/controller/cart-controller')
 const ItemController = require('./src/controller/item-controller')
 
 const app = express()
@@ -8,5 +9,7 @@ const app = express()
 app.use(express.json())
 
 app.post('/item', ItemController.onPost)
+
+app.post('/fill', CartController.onPost)
 
 module.exports = app
