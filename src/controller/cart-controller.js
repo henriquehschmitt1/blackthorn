e76741edce6c8f736fdec2a949516e96cb0c5557
henriquehschmitt <henriquehschmitt@hotmail.com>
2022-02-14 19:40:21 -0300

    feat: added a method to handle post request

--- src/controller/cart-controller.js
+++ src/controller/cart-controller.js
@@ -1,3 +1,17 @@
-class CartController {}
+const CartService = require('../services/cart-service')
+
+class CartController {
+    static async onPost(req, res) {
+        try {
+            const { itemId, quantity } = req.body
+
+            const cart = await CartService.createItem(itemId, quantity)
+
+            res.status(200).send(cart)
+        } catch (error) {
+            res.status(error.statusCode).send(error.message)
+        }
+    }
+}
 
 module.exports = CartController
