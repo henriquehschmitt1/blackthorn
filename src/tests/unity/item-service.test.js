4de6da58f80d02f13ec5df6898608723f674078c
henriquehschmitt <henriquehschmitt@hotmail.com>
2022-02-13 22:03:48 -0300

    feat: added fail case tests for createItem method

--- src/tests/unity/item-service.test.js
+++ src/tests/unity/item-service.test.js
@@ -14,4 +14,87 @@ describe('Item service', () => {
             expect(item.id).toBeTruthy()
         })
     })
+
+    describe('Fail', () => {
+        afterAll(async () => {
+            await Item.deleteMany({})
+        })
+        it('shouldnt be able to create an item into db without name', async () => {
+            const { description, stock, price } = itemMock
+
+            try {
+                await ItemService.createItem('', description, stock, price)
+            } catch (error) {
+                expect(error.statusCode).toBe(400)
+                expect(error.message).toBe('Please inform the name param')
+            }
+        })
+
+        it('shouldnt be able to create an item into db without description', async () => {
+            const { name, stock, price } = itemMock
+
+            try {
+                await ItemService.createItem(name, '', stock, price)
+            } catch (error) {
+                expect(error.statusCode).toBe(400)
+                expect(error.message).toBe('Please inform the description param')
+            }
+        })
+
+        it('shouldnt be able to create an item into db without stock', async () => {
+            const { name, description, price } = itemMock
+
+            try {
+                await ItemService.createItem(name, description, '', price)
+            } catch (error) {
+                expect(error.statusCode).toBe(400)
+                expect(error.message).toBe('Please inform the stock param')
+            }
+        })
+
+        it('shouldnt be able to create an item into db if stock is not a number', async () => {
+            const { name, description, price } = itemMock
+
+            try {
+                await ItemService.createItem(name, description, '500', price)
+            } catch (error) {
+                expect(error.statusCode).toBe(400)
+                expect(error.message).toBe('The stock param should be a number')
+            }
+        })
+
+        it('shouldnt be able to create an item into db without price', async () => {
+            const { name, description, stock } = itemMock
+
+            try {
+                await ItemService.createItem(name, description, stock, '')
+            } catch (error) {
+                expect(error.statusCode).toBe(400)
+                expect(error.message).toBe('Please inform the price param')
+            }
+        })
+
+        it('shouldnt be able to create an item into db if price is not a number', async () => {
+            const { name, description, stock } = itemMock
+
+            try {
+                await ItemService.createItem(name, description, stock, '5')
+            } catch (error) {
+                expect(error.statusCode).toBe(400)
+                expect(error.message).toBe('The price param should be a number')
+            }
+        })
+
+        it('shouldnt be able to create an item into db with a duplicate name', async () => {
+            const { name, description, stock, price } = itemMock
+
+            try {
+                await ItemService.createItem(name, description, stock, price)
+                await ItemService.createItem(name, description, stock, price)
+            } catch (error) {
+                expect(error.statusCode).toBe(400)
+                expect(error.message).toBe('Item already exists in DB')
+            }
+        })
+    })
 })
