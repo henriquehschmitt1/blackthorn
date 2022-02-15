01b9d7b42f49fb3ea82d9b9041f9b3ce0cc75756
henriquehschmitt <henriquehschmitt@hotmail.com>
2022-02-15 17:12:35 -0300

    feat: added a success case test to update an item

--- src/tests/unity/item-service.test.js
+++ src/tests/unity/item-service.test.js
@@ -34,6 +34,19 @@ describe('Item service', () => {
 
             expect(items.length).toBe(2)
         })
+
+        it('should be able to update an items in db', async () => {
+            const { name, description, stock, price } = itemMock
+
+            const item = await ItemService.createItem(name, description, stock, price)
+
+            const updatedItem = await ItemService.updateItems(item._id.toString(), 'samsung', 'android phone', 15, 233)
+
+            expect(updatedItem.name).toBe('samsung')
+            expect(updatedItem.description).toBe('android phone')
+            expect(updatedItem.stock).toBe(15)
+            expect(updatedItem.price).toBe(233)
+        })
     })
 
     describe('Fail', () => {
