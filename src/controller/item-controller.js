d0fc6b9a0da58dca5e2341494579b6097d1e2ded
henriquehschmitt <henriquehschmitt@hotmail.com>
2022-02-15 17:11:25 -0300

    feat: added a method to handle put request

--- src/controller/item-controller.js
+++ src/controller/item-controller.js
@@ -22,6 +22,19 @@ class ItemController {
             res.status(error.statusCode).send(error.message)
         }
     }
+
+    static async onPut(req, res) {
+        try {
+            const { itemId } = req.params
+            const { name, description, stock, price } = req.body
+
+            const items = await ItemService.updateItems(itemId, name, description, stock, price)
+
+            res.status(200).send(items)
+        } catch (error) {
+            res.status(error.statusCode).send(error.message)
+        }
+    }
 }
 
 module.exports = ItemController
