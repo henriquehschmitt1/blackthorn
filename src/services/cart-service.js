3e86d2bb414dc326c9e1496b553f29893323133e
henriquehschmitt <henriquehschmitt@hotmail.com>
2022-02-14 22:07:26 -0300

    feat: added a method to associate a cart to an item by its id and item quantity

--- src/services/cart-service.js
+++ src/services/cart-service.js
@@ -1,3 +1,64 @@
-class CartService {}
+const ValidateService = require('../validations/validate')
+const Item = require('../model/item')
+const Cart = require('../model/cart')
+const CartItem = require('../model/cart-item')
+
+class CartService {
+    static async associate(cartId, itemId, quantity) {
+        ValidateService.validateCartParams(cartId, itemId, quantity)
+        const { price, stock } = await CartService.getItemById(itemId)
+
+        CartService.checkStock(quantity, stock)
+
+        const cartItem = await CartItem.create({
+            itemId,
+            cartId,
+            quantity
+        })
+        const { subtotal } = await CartService.getCartById(cartId)
+
+        const cart = await Cart.findOneAndUpdate({ _id: cartId }, { subtotal: subtotal + price }, { new: true })
+
+        const item = await Item.findOneAndUpdate({ _id: itemId }, { stock: stock - quantity }, { new: true })
+
+        return { cartItem, cart, item }
+    }
+
+    //temporarily update item stock after adding it to cart -> done
+    //if cart has been checked out, item stock will remain the same
+    //if cart has been deleted/updated, check item stock
+    //checkout method to apply taxes, discounts and calculate the total
+
+    static async getItemById(id) {
+        const item = await Item.findById(id)
+        if (!item) {
+            throw {
+                statusCode: 400,
+                message: `The informed id (${id}) does not exists in db`
+            }
+        }
+        return item
+    }
+
+    static async getCartById(id) {
+        const cart = await Cart.findById(id)
+        if (!cart) {
+            throw {
+                statusCode: 400,
+                message: `The informed id (${id}) does not exists in db`
+            }
+        }
+        return cart
+    }
+
+    static checkStock(quantity, stock) {
+        if (quantity > stock) {
+            throw {
+                statusCode: 400,
+                message: 'Quantity is greater than stock, please verify'
+            }
+        }
+    }
+}
 
 module.exports = CartService
