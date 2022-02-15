33c65f789c92cc1d3050bdefc3a10ff4ee1177d7
henriquehschmitt <henriquehschmitt@hotmail.com>
2022-02-15 17:12:02 -0300

    feat: added a method to update a method by id

--- src/services/item-service.js
+++ src/services/item-service.js
@@ -20,6 +20,19 @@ class ItemService {
         return Item.find({}).lean()
     }
 
+    static async updateItems(itemId, name, description, stock, price) {
+        ValidateService.isValidId(itemId, 'itemId')
+        await ItemService.alreadyExists(name)
+
+        const item = await Item.findById(itemId)
+
+        const update = ItemService.setUpdate(item, { name, description, stock, price })
+
+        ValidateService.validateItem(update.name, update.description, update.stock, update.price)
+
+        return Item.findOneAndUpdate({ _id: itemId }, update, { new: true })
+    }
+
     static async alreadyExists(name) {
         const item = await Item.findOne({ name })
 
@@ -29,6 +42,23 @@ class ItemService {
                 message: 'Item already exists in DB'
             }
         }
+    }
+
+    static setUpdate(item, config) {
+        const update = {
+            name: '',
+            description: '',
+            stock: 0,
+            price: 0
+        }
+        const keys = Object.keys(config)
+        for (let key of keys) {
+            update[key] = config[key]
+            if (!config[key]) {
+                update[key] = item[key]
+            }
+        }
+        return update
     }
 }
 
